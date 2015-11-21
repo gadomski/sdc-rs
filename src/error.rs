51e1d9db6c707ef7942cfc18627c0cc97a4f0188
Pete Gadomski <pete.gadomski@gmail.com>
2015-11-21 17:16:15 -0600

    Implement Error for SdcError

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 //! Our custom error types.
 
+use std::error::Error;
+use std::fmt;
 use std::io;
 
 use byteorder;
@@ -8,6 +10,31 @@ use byteorder;
 pub enum SdcError {
     Byteorder(byteorder::Error),
     Io(io::Error),
+}
+
+impl Error for SdcError {
+    fn description(&self) -> &str {
+        match *self {
+            SdcError::Byteorder(ref err) => err.description(),
+            SdcError::Io(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            SdcError::Byteorder(ref err) => Some(err),
+            SdcError::Io(ref err) => Some(err),
+        }
+    }
+}
+
+impl fmt::Display for SdcError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            SdcError::Byteorder(ref err) => write!(f, "Byteorder error: {}", err),
+            SdcError::Io(ref err) => write!(f, "IO error: {}", err),
+        }
+    }
 }
 
 impl From<byteorder::Error> for SdcError {
